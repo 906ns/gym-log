4c0731b3213868fd6a1e161a9871be72e03f2fa8
906ns <mitchy130731@gmail.com>
2026-09-15 23:19:39 +0900

    フェーズ1: PWAの足場とGL仮アイコンを作成

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE = 'gym-log-v1';
+const SHELL = ['./', './index.html', './css/style.css', './js/app.js', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png', './data/exercises.seed.json'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL))));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('gym-log-') && key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
